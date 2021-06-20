55545219c6d892e96409079ca86bb06a268cc43b
vedantgoyal724 <86200636+vedantgoyal724@users.noreply.github.com>
2021-06-20 22:23:46 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a; //variable declaration
+    cout<<"size of int "<<sizeof(a)<<endl;
+    
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    double c;
+    cout<<"size of double "<<sizeof(c)<<endl;
+
+    char d;
+    cout<<"size of char "<<sizeof(d)<<endl;
+    
+    short int si;
+    long int li;
+
+    cout<<"size of short int "<<sizeof(si)<<endl;
+    cout<<"size of long int "<<sizeof(li)<<endl;
+
+    
+    
+    return 0;
+}
